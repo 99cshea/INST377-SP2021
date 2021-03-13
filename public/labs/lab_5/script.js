06171661802d67ffa3c7af5ed34a8e93d5059916
99cshea <51538025+99cshea@users.noreply.github.com>
2021-03-13 04:26:05 -0500

    test

--- public/labs/lab_5/script.js
+++ public/labs/lab_5/script.js
@@ -1,6 +1,6 @@
 function mapInit() {
   // follow the Leaflet Getting Started tutorial here
-  var mymap = L.map('mapid').setView([38.987, -76.943], 13);
+  const mymap = L.map('mapid').setView([38.987, -76.943], 13);
 
   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -11,68 +11,45 @@ function mapInit() {
     accessToken: 'pk.eyJ1IjoiZG9udXRkb29tc2RheSIsImEiOiJja202dDU4MG8wcm53MnFvOXRmbzBkbmV5In0.F-Fpfjwcoo0qwDWVxjV4XQ'
 }).addTo(mymap);
 
-var marker = L.marker([38.987, -76.943]).addTo(mymap);
-  return map;
+  console.log('mymap', mymap)
+
+  return mymap;
 }
 
-/*async function dataHandler(mapObjectFromFunction) {
-const form = document.querySelector('#search-form');
-const form = document.querySelector('#search');
-const form = document.querySelector('.target-list');
-const form = document.querySelector('.reply-message');
+async function dataHandler(mapFromLeaflet) {
+  const form = document.querySelector('#search-form');
+  const search = document.querySelector('#search');
+  const targetList = document.querySelector('.target-list');
 
-const request = await fetch('/api');
-const data = await request.json();
-} */
+  const request = await fetch('/api');
+  const data = await request.json();
+
+  form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    console.log('form submitted');
+    const filtered = data.filter((record) => record.zip.includes(search.value) && record.geocoded_column_1);
+    console.table(filtered);
+
+    filtered.forEach((item) => {
+
+      const longLat = item.geocoded_column_1.coordinates;
+      console.log('markerLongLat', longLat[0], longLat[1]);
+      const marker = L.marker([longLat[1], longLat[0]]).addTo(mapFromLeaflet);
+      
+
+      const appendItem = document.createElement('li');
+      appendItem.classList.add('block');
+      appendItem.classList.add('list-item');
+      appendItem.innerHTML = `<div class="list-header is-size-5>${item.name}</div><address class="is-size-6">${item.name} ${item.address_line_1}</address>`;
+      targetList.append(appendItem);
+
+    });
+  });
+}
 
 async function windowActions() {
   const map = mapInit();
   await dataHandler(map);
 }
 
-window.onload = windowActions;// this code was written with the help of Wes Bos' great tutorial at https://www.youtube.com/watch?v=y4gZMJKAeWs //
-
-const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
-
-const cities = [];
-
-fetch(endpoint)
-.then(blob => blob.json())
-.then(data => cities.push(...data))
-
-function findMatches(wordToMatch, cities) {
-  return cities.filter(place => {
-    const regex = new RegExp(wordToMatch, 'gi');
-    return place.name.match(regex) || place.city.match(regex)
-  });
-}
-
-function clearMatches() {
-  const matchArray = [];
-  return ``
-}
-
-function displayMatches() {
-  const matchArray = findMatches(this.value, cities);
-  const html = matchArray.map(place => {
-    const regex = new RegExp(this.value, 'gi');
-    const cityName = place.city.replace(regex, `<span class="hl">${this.
-    value}</span>`);
-    return `
-    <li>
-      <span class="name"><div class='taco'>${place.name}</div> <address>${place.zip}</address> <div class='citystate'> ${place.city}, ${place.state}</div></span>
-      <span class="category">${place.geocoded_column_1}</span>
-    </li>
-
-    `;
-  }).join('');
-  suggestions.innerHTML = html;
-}
-
-const searchInput = document.querySelector('.search');
-const suggestions = document.querySelector('.suggestions');
-
-searchInput.addEventListener('input', displayMatches);
-
-searchInput.addEventListener('keyup', clearMatches);
-
+window.onload = windowActions;
